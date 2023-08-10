593b198804291defb717be60fb2d6723c72169eb
Sultan Özgür <88083356+sultan-zgr@users.noreply.github.com>
2023-08-10 14:15:20 +0300

    FluentValidation

---
+++ BookStore/BookOperations/DeleteBook/DeleteCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BookStore.BookOperations.DeleteBook
+{
+    public class DeleteBookCommandValidator : AbstractValidator<DeleteBookCommand>
+    {
+        public DeleteBookCommandValidator()
+        {
+            RuleFor(x => x.BookID).NotEmpty().GreaterThan(0);
+        }
+    }
+}
---
+++ BookStore/BookOperations/GetBook/GetBookDetailQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BookStore.BookOperations.GetBook
+{
+    public class GetBookDetailQueryValidator : AbstractValidator<GetBooksQuery>
+    {
+        public GetBookDetailQueryValidator()
+        {
+            RuleFor(x => x.BookID).NotEmpty().GreaterThan(0);
+        }
+    }
+}
